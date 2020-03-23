0a2b43c8eb384a32774d7956db6713f4d95d1e88
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-03-23 17:45:50 +0100

    nlp course part1

---
+++ machine_learning/nlp/chatbots_course/bagofwords.py
@@ -0,0 +1,28 @@
+# importing regex and nltk
+import re, nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+# importing Counter to get word counts for bag of words
+from collections import Counter
+# importing a passage from Through the Looking Glass
+from looking_glass import looking_glass_text
+# importing part-of-speech function for lemmatization
+from part_of_speech import get_part_of_speech
+
+#text = looking_glass_text
+text = 'Mateusz is very nice guy, fascinated with programming!'
+
+cleaned = re.sub('\W+', ' ', text).lower()
+tokenized = word_tokenize(cleaned)
+
+stop_words = stopwords.words('english')
+filtered = [word for word in tokenized if word not in stop_words]
+
+normalizer = WordNetLemmatizer()
+normalized = [normalizer.lemmatize(token, get_part_of_speech(token)) for token in filtered]
+
+#print(normalized)
+
+bag_of_looking_glass_words = Counter(normalized)
+print(bag_of_looking_glass_words)
---
+++ machine_learning/nlp/chatbots_course/text_gen/document3.py
@@ -0,0 +1,3 @@
+training_doc3 = """
+All of us are susceptible to these obsessions of the mind – whether we run a technology startup or are working our way up the ranks of the corporate hierarchy or have fallen madly in love. The more creative we are, the easier it is to lose the thread that guides us.
+"""
